c43637d34b400031376a05daab64c0d775b2e695
EfrainOlivares <efrain@rightscale.com>
2014-12-11 22:16:53 +0000

    saving work

--- lib/scru.rb
+++ lib/scru.rb
@@ -7,7 +7,7 @@ class Scru < Thor
 
   def get_metadata(file)
     raise ArgumentError.new("File #{file} does not exist") unless ::File.exists?(file)
- 
+    yaml_metadata = nil
     File.open(file) do |file|
       in_metadata = false
       metadata = StringIO.new
@@ -21,9 +21,10 @@ class Scru < Thor
         end
       end
       metadata.rewind
-      pp YAML.load(metadata, file.path) || {}
+      yaml_metadata = YAML.load(metadata) || {}
     end
-
+    pp yaml_metadata
+    yaml_metadata
   end
 
   desc "list", "List RightScripts"
@@ -43,6 +44,7 @@ class Scru < Thor
     :aliases => "-n"
   def upload(file)
     raise ArgumentError.new("File #{file} does not exist") unless ::File.exists?(file)
+
   end
 
   desc "download <file>", "Download RightScript to file"
@@ -57,13 +59,39 @@ class Scru < Thor
     raise ArgumentError.new("Either name or id must be supplied") unless options[:name] || options[:id]
   end
 
+  desc "has_meatadata?", "Test if a script has a metadata block"
+  def has_metadata?( filename )
+    raise ArgumentError.new("File #{filename} does not exist") unless ::File.exists?(filename)
+
+    File.open(filename) do |file|
+      has_metadata = false
+      in_metadata = false
+      file.each_line do |line|
+        if in_metadata
+          has_metadata = true if line.start_with?('# ...')
+        else
+           in_metadata = true if line.start_with?('# ---')
+        end
+      end
+      puts "The file #{filename} #{has_metadata ? " has " : "does not have "} metadata"
+      has_metadata
+    end
+  end
+
+
   private
   def list_files(dir, filter = "*")
     Dir.glob(File.expand_path("#{dir}/##{filter}")).each do |file| 
       puts file
     end
   end
+ 
+  def push_to_rightscale(file)
+    puts "Pushing #{file} up to RightScale"
+  end
 
-
+  def get_from_rightcale( script_name )
+    puts "Get #{script_name} from RightScale"
+  end 
 end
 
---
+++ meta.sh
@@ -0,0 +1,4 @@
+# ---
+# RightScript Name: Name of RightScript
+# Description: This is the description of the RightScript
+# ...
